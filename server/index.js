042b34edacb90b33e44bbc5c1195e14ba740fd0c
SagarKarathiya <sagar_karathiya@yahoo.com>
2024-09-18 14:37:07 +0530

    added import statement

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import session from 'express-session';
@@ -22,7 +23,7 @@ dotenv.config();
 
 initDatabase();
 // const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const app = express();
 
 const transporter = nodemailer.createTransport({
